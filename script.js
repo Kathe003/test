04d0810e3704da11fc50f0a2c46b135d2047e80d
Kathe003 <145651863+Kathe003@users.noreply.github.com>
2024-12-02 18:05:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Fonction JavaScript qui change le texte du paragraphe
+
 function changerTexte() {
-    // Récupère l'élément avec l'ID 'message'
+
     var paragraphe = document.getElementById('message');
-    
-    // Change le texte du paragraphe
+  
     paragraphe.innerHTML = "Le texte a été changé !";
 }
